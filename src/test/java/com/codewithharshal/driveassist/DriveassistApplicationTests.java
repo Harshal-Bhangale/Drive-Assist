52db55fcaf5774e61389124bf1f61def2f56f73a
Harshal Bhangale <99475142+Harshal-Bhangale@users.noreply.github.com>
2025-08-19 19:44:19 +0530

    update

---
+++ src/test/java/com/codewithharshal/driveassist/DriveassistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithharshal.driveassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveassistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
