7161c017ba6a49851daa4d373b1f0c8fc104c965
Harshal Bhangale <99475142+Harshal-Bhangale@users.noreply.github.com>
2025-08-19 19:42:09 +0530

    update

---
+++ driveassist/driveassist/src/main/java/com/codewithharshal/driveassist/DriveassistApplication.java
@@ -0,0 +1,13 @@
+package com.codewithharshal.driveassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveassistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveassistApplication.class, args);
+	}
+
+}
