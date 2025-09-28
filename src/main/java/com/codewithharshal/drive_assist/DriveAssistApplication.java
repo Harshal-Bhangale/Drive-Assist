86ff1deb48899c4bc64cdaf6e1c6e69d499916fd
Harshal Bhangale <99475142+Harshal-Bhangale@users.noreply.github.com>
2025-09-28 12:39:35 +0530

    Project Structure

---
+++ src/main/java/com/codewithharshal/drive_assist/DriveAssistApplication.java
@@ -0,0 +1,13 @@
+package com.codewithharshal.drive_assist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveAssistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codewithharshal/drive_assist/DriveAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithharshal.drive_assist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
